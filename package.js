00485d5ac74f4177e5866a5fe904957a6f442939
Max Malm <benjick@dumfan.net>
2015-04-30 14:27:34 +0200

    added git url

--- package.js
+++ package.js
@@ -3,10 +3,7 @@ Package.describe({
   version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/rymd/humblebundle.git',
   documentation: 'README.md'
 });
 
